bc89e95634c7b0283d2b92092feebd0b6073d978
bewisessong <bewisessong@gmail.com>
2022-03-16 22:48:20 +0900

    3/16(수) - Upload 페이지, Form, onChangeHandler 생성

---
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+import { Typography, Button, Form, Input } from 'antd';
+
+const { Title } = Typography;
+const { TextArea } = Input;
+
+function UploadProductPage() {
+
+    const [title, setTitle] = useState("")
+    const [Description, setDescription] = useState("")
+    const [Price, setPrice] = useState(0)
+    const [Continent, setContinent] = useState(1)
+    const [Images, setImages] = useState([])
+
+    const titleChangeHandler = (event) => {
+        setTitle(event.currentTarget.value)
+    }
+    
+    const descriptionChangeHandler = (event) => {
+        setDescription(event.currentTarget.value)
+    }
+
+    const priceChangeHandler = (event) => {
+        setPrice(event.currentTarget.value)
+    }
+
+  return (
+    <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
+        <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
+            <Title level={2}>여행 상품 업로드</Title>
+        </div>
+
+        <Form>
+            {/* Drop Zone */}
+            <br />
+            <br />
+            <label>이름</label>
+            <Input onChange={titleChangeHandler} value={title} />
+            <br />
+            <br />
+            <label>설명</label>
+            <TextArea onChange={descriptionChangeHandler} value={Description}/>
+            <br />
+            <br />
+            <label>가격($)</label>
+            <Input type="number" onChange={priceChangeHandler} value={Price} />
+            <select>
+                <option></option>
+            </select>
+            <br />
+            <br />
+            <Button>
+                확인
+            </Button>
+        </Form>
+    </div>
+  )
+}
+
+export default UploadProductPage

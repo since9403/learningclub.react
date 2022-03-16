6c40beb087da5f3648383eb89ec2c97ea110d681
bewisessong <bewisessong@gmail.com>
2022-03-16 23:26:44 +0900

    03/16(수) - React Dropzone 적용 진행 중

--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -1,8 +1,20 @@
 import React, { useState } from 'react'
 import { Typography, Button, Form, Input } from 'antd';
 
+import FileUpload from '../../utils/FileUpload';
+
 const { Title } = Typography;
 const { TextArea } = Input;
+
+const Continents = [
+    { key: 1, value: "Africa" },
+    { key: 2, value: "Europe" },
+    { key: 3, value: "Asia" },
+    { key: 4, value: "North America" },
+    { key: 5, value: "South America" },
+    { key: 6, value: "Australia" },
+    { key: 7, value: "Antarctica" }
+]
 
 function UploadProductPage() {
 
@@ -24,6 +36,10 @@ function UploadProductPage() {
         setPrice(event.currentTarget.value)
     }
 
+    const continentChangeHandler = (event) => {
+        setContinent(event.currentTarget.value)
+    }
+
   return (
     <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
         <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
@@ -32,6 +48,8 @@ function UploadProductPage() {
 
         <Form>
             {/* Drop Zone */}
+            <FileUpload />
+
             <br />
             <br />
             <label>이름</label>
@@ -44,8 +62,14 @@ function UploadProductPage() {
             <br />
             <label>가격($)</label>
             <Input type="number" onChange={priceChangeHandler} value={Price} />
-            <select>
-                <option></option>
+            <select onChange={continentChangeHandler} value={Continent} >
+                {
+                    Continents.map((item) => (
+                        <option key={item.key} value={item.key}>
+                            {item.value}
+                        </option>
+                    ))
+                }
             </select>
             <br />
             <br />
